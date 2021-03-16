9c430c710327353ca774d700c088d0ede6229063
neyasbit <kobzevjob@gmail.com>
2021-03-16 22:16:02 +0700

    connected with data

--- app/src/main/java/com/example/roomwordsample/MainActivity.kt
+++ app/src/main/java/com/example/roomwordsample/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.roomwordsample
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.roomwordsample.data.WordRoomDatabase
+import com.example.roomwordsample.model.Word
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+
+    private val newWordActivityRequest = 1
+    private val wordViewModel: WordViewModel by viewModels {
+        WordViewModelFactory((application as WordsApplication).repository)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,5 +25,27 @@ class MainActivity : AppCompatActivity() {
         val adapter = WordListAdapter()
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(this)
+
+        wordViewModel.allWords.observe( this) { words ->
+            words.let { adapter.submitList(it)}
+        }
+
+        val fab = findViewById<FloatingActionButton>(R.id.fab)
+        fab.setOnClickListener {
+            val intent = Intent(this@MainActivity, NewWordActivity::class.java)
+            startActivityForResult(intent, newWordActivityRequest)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == newWordActivityRequest && resultCode == RESULT_OK) {
+            data?.getStringExtra(NewWordActivity.EXTRA_REPLY)?.let { reply ->
+                val word = Word(0, reply)
+                wordViewModel.insert(word)
+            }
+        } else {
+            Toast.makeText(this, R.string.empty_not_saved, Toast.LENGTH_SHORT).show()
+        }
     }
 }
